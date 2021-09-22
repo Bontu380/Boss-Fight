f6c4d9162ced9f4204d24af399a1c32c75761cd6
Bontu380 <onyldrm@hotmail.com>
2021-09-22 20:18:51 +0300

    Line renderer links

---
+++ Assets/Scripts/Mace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mace : MonoBehaviour
+{
+
+    public Transform junctionMaceRigidbody;
+    public Transform junctionHandleRigidbody;
+
+    public LineRenderer lineRenderer;
+
+
+    private void LateUpdate()
+    {
+        lineRenderer.SetPosition(0, junctionHandleRigidbody.position);
+        lineRenderer.SetPosition(1, junctionMaceRigidbody.position);    
+    }
+}
